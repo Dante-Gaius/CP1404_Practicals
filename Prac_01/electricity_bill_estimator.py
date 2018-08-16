9263cc686e92dd350f934c959d846ffd5962854a
Dante-Gaius <dante.gaius@my.jcu.edu>
2018-08-16 21:28:39 +1000

    Add Prac Folders to GitHub

--- Prac_01/electricity_bill_estimator.py
+++
@@ -1,7 +0,0 @@
-price = float(input("Enter cents per KWh: "))
-daily_use = float(input("Enter daily use in KWh: "))
-days = int(input("Enter number of billing days: "))
-
-bill = (price / 100) * daily_use * days
-
-print("Estimated bill: $ {:.2f}".format(bill))
