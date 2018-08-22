4d524025969ac8ca5dcd7afaef9ea279fc354f34
Dante-Gaius <dante.gaius@my.jcu.edu>
2018-08-22 12:58:17 +1000

    Add List Warm up and total income to github

---
+++ cp1404practicals/Prac_04/list_warmup.py
@@ -0,0 +1,23 @@
+numbers = [3, 1, 4, 1, 5, 9, 2]
+print(numbers[0])
+# Answer = 3
+print(numbers[-1])
+# Answer = 2
+print(numbers[3])
+# Answer = 5
+print(numbers[:-1])
+# Answer = [3, 1, 4, 1, 5, 9]
+print(numbers[3:4])
+# Answer = [1]
+print(5 in numbers)
+# Answer = True
+print(7 in numbers)
+# Answer = False
+print("3" in numbers)
+# Answer = False
+print(numbers + [6, 5, 3])
+# Answer = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers_2 = ["ten", 3, 1, 4, 1, 5, 9, 2, 1]
+print(numbers_2[2:])
+print(9 in numbers_2)
